e69bb3bcefdeead075121432f06b0004527e0c48
TCat093 <TCat093@163.com>
2019-09-15 20:11:59 +0800

    ssinit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'antd/dist/antd.css';
+import axios from 'utils/axios.js'
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+React.Component.prototype.$axios = axios
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
